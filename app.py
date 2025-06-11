d0ebe8bd0494a28ed9fde0c412385b9e7419b81c
navles45 <41605172+navles45@users.noreply.github.com>
2025-06-11 11:07:35 +0800

    fastapi

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from typing import Union
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
